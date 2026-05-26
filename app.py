3a6cbd721439877e8dbfd9ca6928206b3a7f1908
Sriya Priyadarshani <sriyapriyadarshani9@gmail.com>
2026-05-26 19:09:43 +0530

    updated input field

--- app.py
+++ app.py
@@ -2,5 +2,17 @@ import streamlit as st
 
 def main():
   st.header("This is title.")
-  st.text_input("enter your name:")
+  name=st.text_input("enter your name:")
+
+
+  col1 , col2 = st.columns(2, gap="small")
+
+  with col1:
+    if st.button("Hi", type="primary", key='btn1', width='stretch'):
+      print("hi", name)
+
+  with col2:
+    if st.button("Bye", type="secondary", key='btn2', width='stretch'):
+      print("bye", name)    
+      
 main()
