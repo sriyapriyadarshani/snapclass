5f25058642378b8f49ce061ce8b8d8cd493a11e3
Sriya Priyadarshani <sriyapriyadarshani9@gmail.com>
2026-05-26 20:09:44 +0530

    updated input field

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import streamlit as st
 
+
+from src.screens.home_screen import home_screen
+from src.screens.teacher_screen import teacher_screen
+from src.screens.student_screen import student_screen
+
+
 def main():
-  st.header("This is title.")
-  name=st.text_input("enter your name:")
+   if 'login_type' not in st.session_state:
+      st.session_state['login_type'] = None  
 
+   match st.session_state['login_type']:
+      case 'teacher':
+         teacher_screen()
 
-  col1 , col2 = st.columns(2, gap="small")
+      case 'student': 
+        student_screen()
 
-  with col1:
-    if st.button("Hi", type="primary", key='btn1', width='stretch'):
-      print("hi", name)
-
-  with col2:
-    if st.button("Bye", type="secondary", key='btn2', width='stretch'):
-      print("bye", name)    
-      
+      case None:
+         home_screen()     
 main()
