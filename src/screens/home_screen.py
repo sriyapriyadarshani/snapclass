b7f6a262268691266d32a93d8cfd5e77ec2928c5
Sriya Priyadarshani <sriyapriyadarshani9@gmail.com>
2026-05-27 22:02:46 +0530

    updated input field

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from src.components.header import header_home
+from src.components.footer import footer_home
 from src.ui.base_layout import style_base_layout,style_background_home
 
 def home_screen():
@@ -11,14 +12,21 @@ def home_screen():
 
 
 
-    col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2,gap="large")
 
 
     with col1:
-        if st.button('Teacher Portal'):
-            st.session_state['login_type'] = 'teacher'
-            st.rerun()
-    with col2:
-        if st.button('Student Portal'):
+        st.header("I'm Student")
+        st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
+        if st.button('Student Portal', type='primary'):
             st.session_state['login_type'] = 'student'
-            st.rerun()        
+            st.rerun() 
+        
+    with col2:
+        st.header("I'm Teacher")
+        st.image("https://i.ibb.co/CsmQQV6X/mascot-prof.png", width=145)
+        if st.button('Teacher Portal', type='primary'):
+            st.session_state['login_type'] = 'teacher'
+            st.rerun()      
+
+    footer_home()        
